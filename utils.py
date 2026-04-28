cc1531bf6f0ecdffddf067d3f61bda8162d2e425
Kartik Nair <kartikn107@gmail.com>
2026-04-28 22:25:59 +0530

    Initial commit

--- utils.py
+++ utils.py
@@ -3,9 +3,6 @@ import json
 import logging
 from openai import OpenAI
 
-# Set up OpenAI client
-# the newest OpenAI model is "gpt-4o" which was released May 13, 2024.
-# do not change this unless explicitly requested by the user
 OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
 openai = OpenAI(api_key=OPENAI_API_KEY)
 
